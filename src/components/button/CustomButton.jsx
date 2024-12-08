f44f5b71cd05085d7fc32bbce9cdefa20af03e67
Siddhant Shrestha <siddhantshrestha04@gmail.com>
2024-12-08 18:59:59 +0545

    Buttons and icons

---
+++ src/components/button/CustomButton.jsx
@@ -0,0 +1,67 @@
+import { Button } from "@mui/material";
+import React from "react";
+import "./button.style.css";
+import DeleteOutlineSharpIcon from "@mui/icons-material/DeleteOutlineSharp";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+
+const CustomButton = () => {
+  return (
+    <div>
+      <section id="variant-section" className="button-section">
+        <h3>Button Variant</h3>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Click Me</Button>
+        <Button variant="outline">Outlined</Button>
+      </section>
+
+      <section id="colour-section" className="button-section">
+        <h3>Color Variant</h3>
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button sx={{ background: "white", color: "#81BFDA" }}>Random</Button>
+      </section>
+      <section id="size-button" className="button-section">
+        <h3>Button Size</h3>
+        <Button variant="contained" size="small">
+          small
+        </Button>
+        <Button variant="contained">default medium</Button>
+        <Button variant="contained" size="large">
+          HUGE
+        </Button>
+      </section>
+      <section id="icon-button" className="button-section">
+        <h3>Button with icon</h3>
+        <Button
+          variant="outlined"
+          color="error"
+          startIcon={<DeleteOutlineSharpIcon />}
+        >
+          Delete Product
+        </Button>
+        <Button
+          variant="outlined"
+          color="success"
+          endIcon={<EditOutlinedIcon />}
+        >
+          Edit Product
+        </Button>
+      </section>
+    </div>
+  );
+};
+
+export default CustomButton;
